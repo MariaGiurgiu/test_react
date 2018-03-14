1b0b15e0d0c65af21446bc1025220f791e1f6576
Corneliu <vesacorneliu@yahoo.com>
2018-03-14 09:22:05 +0200

    Remove superfluous comments

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// TODO crează o aplicație NodeJS cu Express
 let express = require('express');
 let fs = require ('fs');
 let app = express();
@@ -11,16 +10,11 @@ app.set('port', 3000);
 
 app.use('/MyComponent.js', express.static(path.join('MyComponent.js')));
 
-// TODO crează un fișier test.html care să conțină un element DOM cu id-ul "react"
-
-// TODO crează o componentă React care să conțină textul "Hello, World"
-
 // TODO Crează un endpoint care să întoarcă un html care va face, apoi, redner client side a componentei React de mai sus
 app.get('/clientsidereact', function (req,res) {
 	res.sendFile(__dirname + '/test.html');
 });
 
-// TODO Crează un endpoint care să întoarcă un html care să conțină deja o componentă React foarte simplă (aceeași) randată server side
 app.get('/serversidereact', function (req,res) {
 	let component = React.createElement(c.component, null)
 	let html = ReactDOMServer.renderToString(component);
